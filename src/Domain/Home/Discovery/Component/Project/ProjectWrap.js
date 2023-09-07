47150792f316d4348fd5d820693d769684ae63f6
hdy86 <dyhong86@naver.com>
2023-09-07 17:43:24 +0900

    refacotr: 디스커버리 검색(과제정보분석) 버튼 위치 변경

--- src/Domain/Home/Discovery/Component/Project/ProjectWrap.js
+++ src/Domain/Home/Discovery/Component/Project/ProjectWrap.js
@@ -40,49 +40,51 @@ export default function ProjectWrap(props) {
         </div>
       </div>
       {(!fold)
-        ? <div className='search_detail_wrap pt-4 mt-4 border-top-placeholder'>
-          <dl>
-            <dt>과제명</dt>
-            <dd>
-              <InputTextXBtn id={'project'} title={'과제명'} value={''} onChange={() => {}} />
-            </dd>
-            <dt className='flex items-center gap-1'>
+        ? <>
+          <div className='search_detail_wrap pt-4 mt-4 border-top-placeholder'>
+            <dl>
+              <dt>과제명</dt>
+              <dd>
+                <InputTextXBtn id={'project'} title={'과제명'} value={''} onChange={() => {}} />
+              </dd>
+              <dt className='flex items-center gap-1'>
               키워드(한글)
-              <div className='tooltip_wrap' tabIndex={0}>
-                <img src={icQuestion} alt='한글 키워드 설명글' className='w-6' />
-                <span className='tooltip_style04 min-w-20'>콤마(,)로 구분</span>
-              </div>
-            </dt>
-            <dd>
-              <InputTextXBtn id={'keywordsKo'} title={'한글 키워드'} value={''} onChange={() => {}} />
-            </dd>
-            <dt className='flex items-center gap-1'>
+                <div className='tooltip_wrap' tabIndex={0}>
+                  <img src={icQuestion} alt='한글 키워드 설명글' className='w-6' />
+                  <span className='tooltip_style04 min-w-20'>콤마(,)로 구분</span>
+                </div>
+              </dt>
+              <dd>
+                <InputTextXBtn id={'keywordsKo'} title={'한글 키워드'} value={''} onChange={() => {}} />
+              </dd>
+              <dt className='flex items-center gap-1'>
               키워드(영문)
-              <div className='tooltip_wrap' tabIndex={0}>
-                <img src={icQuestion} alt='영문 키워드 설명글' className='w-6' />
-                <span className='tooltip_style04 min-w-20'>콤마(,)로 구분</span>
-              </div>
-            </dt>
-            <dd>
-              <InputTextXBtn id={'keywordsEn'} title={'영문 키워드'} value={''} onChange={() => {}} />
-            </dd>
-            <dt>연구 목표</dt>
-            <dd>
-              <InputTextXBtn id={'subject'} title={'연구 목표'} value={''} onChange={() => {}} />
-            </dd>
-            <dt>연구 내용</dt>
-            <dd>
-              <InputTextXBtn id={'content'} title={'연구 내용'} value={''} onChange={() => {}} />
-            </dd>
-            <dt>기대 효과</dt>
-            <dd>
-              <InputTextXBtn id={'benefit'} title={'기대 효과'} value={''} onChange={() => {}} />
-            </dd>
-          </dl>
-        </div>
+                <div className='tooltip_wrap' tabIndex={0}>
+                  <img src={icQuestion} alt='영문 키워드 설명글' className='w-6' />
+                  <span className='tooltip_style04 min-w-20'>콤마(,)로 구분</span>
+                </div>
+              </dt>
+              <dd>
+                <InputTextXBtn id={'keywordsEn'} title={'영문 키워드'} value={''} onChange={() => {}} />
+              </dd>
+              <dt>연구 목표</dt>
+              <dd>
+                <InputTextXBtn id={'subject'} title={'연구 목표'} value={''} onChange={() => {}} />
+              </dd>
+              <dt>연구 내용</dt>
+              <dd>
+                <InputTextXBtn id={'content'} title={'연구 내용'} value={''} onChange={() => {}} />
+              </dd>
+              <dt>기대 효과</dt>
+              <dd>
+                <InputTextXBtn id={'benefit'} title={'기대 효과'} value={''} onChange={() => {}} />
+              </dd>
+            </dl>
+          </div>
+          <Button name="과제 정보 분석" icon={icSearch} className="gap-2 mt-6 mx-auto py-3 px-6.5 rounded-3xl text-base font-bold btn_style03" />
+        </>
         : ''
       }
-      <Button name="과제 정보 분석" icon={icSearch} className="gap-2 mt-6 mx-auto py-3 px-6.5 rounded-3xl text-base font-bold btn_style03" />
       {(popup) ? <ExcelPopup setPopup={setPopup} /> : ''}
     </>
   );
