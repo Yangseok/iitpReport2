c7414b909c2a9f2aa6fc65f1116c7ea49cb79bae
kimyunsik <dbstlr260@naver.com>
2023-09-26 14:08:02 +0900

    feat: ICT 트렌드 API 코드 추가

--- src/Domain/Home/ICTTrend/API/Call.js
+++ src/Domain/Home/ICTTrend/API/Call.js
@@ -2,9 +2,6 @@ import API from 'Utill/API';
 
 // ICT 트렌드
 
-
-// ICT 키워드 추이 
-
 export const ictSearchWordCloud = async (category, label) => {
   console.log('dataparams',category, label);
   let params = { category: category, label: label};
