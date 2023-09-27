c04685c56170d38e83b728406c0a73881a32583d
yangseok Lee <ysl8607@naver.com>
2023-09-27 11:02:05 +0900

    feat: 수요뱅킹 병합수요관련 작업중

--- src/Config/Router.js
+++ src/Config/Router.js
@@ -105,7 +105,7 @@ export default function Router() {
         <Route path='/demandbanking' element={<DemandBanking />} />
         <Route path='/demandbanking/result' element={<DemandBankingResult />} />
         <Route path='/demandbanking/view/:noticeId/:surveyId' element={<DemandBankingView />} />
-        <Route path='/demandbanking/merge/:id' element={<DemandBankingMerge />} />
+        <Route path='/demandbanking/merge/:surveyId' element={<DemandBankingMerge />} />
         {list.map((e,i) => {
           return <Route key={i} path={'/demandbanking/file/result/' + e.path} element={<ListWrap filterKey={e.filterKey} searchDetailKey={e.searchDetailKey} />} />;
         })}
