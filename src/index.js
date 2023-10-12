1cbd06d7cf8832dff4135c4eb07a1f2b151fa54d
yangseok Lee <ysl8607@naver.com>
2023-10-12 17:29:41 +0900

    fix: ict트렌드 하단 리스트에 api를 통합검색 api가 아닌 36번 ICT 결과목록 api 호출로 변경

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={pst}>
-      <Router />
-    </PersistGate>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={pst}>
+        <Router />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
